4a554403bca19ced067a9abcc6043d10754150d0
Vy-DPM225505 <vy_dpm225505@student.agu.edu.vn>
2025-09-16 12:55:20 +0700

    Add files via upload

---
+++ Real10_Adapter/Program.cs
@@ -0,0 +1,26 @@
+﻿using Real10_Adapter;
+using System;
+namespace Real10_Adapter
+{
+    /// <summary>
+    /// Adapter Design Pattern
+    /// </summary>
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Non-adapted chemical compound
+            Compound unknown = new Compound();
+            unknown.Display();
+            // Adapted chemical compounds
+            Compound water = new RichCompound("Water");
+            water.Display();
+            Compound benzene = new RichCompound("Benzene");
+            benzene.Display();
+            Compound ethanol = new RichCompound("Ethanol");
+            ethanol.Display();
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+}
