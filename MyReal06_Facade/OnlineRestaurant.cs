b004537d5036113e2812fe6ddd663ca4e5c8b6d2
Vy-DPM225505 <vy_dpm225505@student.agu.edu.vn>
2025-09-16 12:37:55 +0700

    Add files via upload

--- MyReal06_Facade/OnlineRestaurant.cs
+++ MyReal06_Facade/OnlineRestaurant.cs
@@ -1,1 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace MyReal06_Facade
+{
+    public class OnlineRestaurant
+    {
+        private readonly List<Order> _cart;
+        public OnlineRestaurant()
+        {
+            _cart = new List<Order>();
+        }
+        public void AddOrderToCart(Order order)
+        {
+            _cart.Add(order);
+        }
+        public void CompleteOrders()
+        {
+            Console.WriteLine("Orders completed. Dispatch in progress...");
+        }
+    }
+}
---
+++ MyReal06_Facade/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace MyReal06_Facade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var restaurant = new OnlineRestaurant();
+            var shippingService = new ShippingService();
+            var chickenOrder = new Order() { DishName = "Chicken with rice", DishPrice = 20.0, User = "User1", ShippingAddress = "Random street 123" };
+            var sushiOrder = new Order() { DishName = "Sushi", DishPrice = 52.0, User = "User2", ShippingAddress = "More random street 321" };
+            restaurant.AddOrderToCart(chickenOrder);
+            restaurant.AddOrderToCart(sushiOrder);
+            restaurant.CompleteOrders();
+            shippingService.AcceptOrder(chickenOrder);
+            shippingService.CalculateShippingExpenses();
+            shippingService.ShipOrder();
+            shippingService.AcceptOrder(sushiOrder);
+            shippingService.CalculateShippingExpenses();
+            shippingService.ShipOrder();
+            Console.ReadLine();
+        }
+    }
+}
