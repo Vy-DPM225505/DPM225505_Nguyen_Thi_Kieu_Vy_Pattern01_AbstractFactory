deebd3f5611ce02108d8ee5adf7798c5436e2cfb
Vy-DPM225505 <vy_dpm225505@student.agu.edu.vn>
2025-09-16 12:39:52 +0700

    Add files via upload

---
+++ MyReal08_Composite/Component.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyReal08_Composite
+{
+    public abstract class GiftBase
+    {
+        protected string name;
+        protected int price;
+
+        public GiftBase(string name, int price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+
+        public abstract int CalculateTotalPrice();
+    }
+    public interface IGiftOperations
+    {
+        void Add(GiftBase gift);
+        void Remove(GiftBase gift);
+    }
+
+}
---
+++ MyReal08_Composite/Leaf.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyReal08_Composite
+{
+    public class SingleGift : GiftBase
+    {
+        public SingleGift(string name, int price)
+            : base(name, price)
+        {
+        }
+
+        public override int CalculateTotalPrice()
+        {
+            Console.WriteLine($"{name} with the price {price}");
+
+            return price;
+        }
+    }
+
+}
