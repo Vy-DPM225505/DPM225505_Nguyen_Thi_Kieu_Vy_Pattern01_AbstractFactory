a95d25c248dbd683014307a2f09e8e90b745fb25
Vy-DPM225505 <vy_dpm225505@student.agu.edu.vn>
2025-09-16 12:37:11 +0700

    Add files via upload

--- MyReal05_Singleton/Program.cs
+++ MyReal05_Singleton/Program.cs
@@ -1,1 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 
+namespace MyReal05_Singleton
+{
+    /// <summary>
+
+    /// MainApp startup class for Real-World 
+
+    /// Singleton Design Pattern.
+
+    /// </summary>
+
+    class MainApp
+
+    {
+        /// <summary>
+
+        /// Entry point into console application.
+
+        /// </summary>
+
+        static void Main()
+        {
+            LoadBalancer b1 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b2 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b3 = LoadBalancer.GetLoadBalancer();
+            LoadBalancer b4 = LoadBalancer.GetLoadBalancer();
+
+            // Same instance?
+
+            if (b1 == b2 && b2 == b3 && b3 == b4)
+            {
+                Console.WriteLine("Same instance\n");
+            }
+
+            // Load balance 15 server requests
+
+            LoadBalancer balancer = LoadBalancer.GetLoadBalancer();
+            for (int i = 0; i < 15; i++)
+            {
+                string server = balancer.Server;
+                Console.WriteLine("Dispatch Request to: " + server);
+            }
+
+            // Wait for user
+
+            Console.ReadKey();
+        }
+    }
+}
