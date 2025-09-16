c697fc6d64dc5871e9c96c6c1c642cc29d9e1f52
Vy-DPM225505 <vy_dpm225505@student.agu.edu.vn>
2025-09-16 12:41:43 +0700

    Add files via upload

---
+++ MyReal10_Adapter/Blue.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyReal09_Bridge
+{
+    class Blue : Color
+    {
+        public string GetColor()
+        {
+            return "Blue";
+        }
+    }
+}
---
+++ MyReal10_Adapter/Shape.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyReal09_Bridge
+{
+    abstract class Shape
+    {
+        public Color color { get; set; }
+
+        public string GetColor()
+        {
+            return color.GetColor();
+        }
+    }
+}
