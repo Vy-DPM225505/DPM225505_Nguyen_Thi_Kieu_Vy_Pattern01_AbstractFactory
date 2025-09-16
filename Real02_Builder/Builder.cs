f6d0b0511cac7879f861e0da4e5b7585757c3639
Vy-DPM225505 <vy_dpm225505@student.agu.edu.vn>
2025-09-16 12:32:00 +0700

    Add files via upload

--- Real02_Builder/Builder.cs
+++ Real02_Builder/Builder.cs
@@ -1,1 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Real02_Builder
+{
+    abstract class VehicleBuilder
+    {
+        protected Vehicle vehicle;
+        // Gets vehicle instance
+        public Vehicle Vehicle
+        {
+            get { return vehicle; }
+        }
+        // Abstract build methods
+        public abstract void BuildFrame();
+        public abstract void BuildEngine();
+        public abstract void BuildWheels();
+        public abstract void BuildDoors();
+    }
+
+}
