0f421a5c911bffc18409dd9572bd7de901bf7da8
Vy-DPM225505 <vy_dpm225505@student.agu.edu.vn>
2025-09-16 12:44:58 +0700

    Add files via upload

--- Pattern05_Singleton/Program.cs
+++ Pattern05_Singleton/Program.cs
@@ -1,1 +1,23 @@
-
+﻿using System;
+namespace Pattern05_Singleton
+{
+    /// <summary>
+    /// Singleton Design Pattern
+    /// </summary>
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Constructor is protected -- cannot use new
+            Singleton s1 = Singleton.Instance();
+            Singleton s2 = Singleton.Instance();
+            // Test for same instance
+            if (s1 == s2)
+            {
+                Console.WriteLine("Objects are the same instance");
+            }
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+}
