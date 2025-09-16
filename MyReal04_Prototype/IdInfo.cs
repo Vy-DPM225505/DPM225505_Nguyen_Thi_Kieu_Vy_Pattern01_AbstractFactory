9e4559cd9db3f090228ea22c5c2a78311bdbb927
Vy-DPM225505 <vy_dpm225505@student.agu.edu.vn>
2025-09-16 12:36:16 +0700

    Add files via upload

--- MyReal04_Prototype/IdInfo.cs
+++ MyReal04_Prototype/IdInfo.cs
@@ -1,1 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace MyReal04_Prototype
+{
+    public class IdInfo
+    {
+        public int IdNumber;
+
+        public IdInfo(int idNumber)
+        {
+            this.IdNumber = idNumber;
+        }
+    }
+}
---
+++ MyReal04_Prototype/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyReal04_Prototype
+{
+    public class Person
+    {
+        public int Age;
+        public DateTime BirthDate;
+        public string Name;
+        public IdInfo IdInfo;
+
+        //Các object con bên trong chỉ được copy reference. Nghĩa là chỉ nhân bản được value type.
+        public Person ShallowCopy()
+        {
+            return (Person)this.MemberwiseClone();
+        }
+        public Person DeepCopy()
+        {
+            Person clone = (Person)this.MemberwiseClone();
+            clone.IdInfo = new IdInfo(IdInfo.IdNumber);
+            clone.Name = String.Copy(Name);
+            return clone;
+        }
+    }
+}
